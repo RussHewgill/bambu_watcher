435bc823baf4e40845c0cd0982cc69406193faf6
RussHewgill <RussHewgill@users.noreply.github.com>
2024-05-08 10:27:39 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+#![allow(unused_mut)]
+#![allow(dead_code)]
+#![allow(unused_doc_comments)]
+#![allow(unused_labels)]
+#![allow(unexpected_cfgs)]
+
+fn main() {
+    println!("Hello, world!");
+}
